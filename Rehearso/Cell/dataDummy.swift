1b76fc4cd6b7d2141c488f95d112edd6bf10cc8d
Mohammad Sulthan <37440405+mohsulthana@users.noreply.github.com>
2021-08-07 14:41:27 +0700

    fix insert data to coredata issue

--- Rehearso/Cell/dataDummy.swift
+++ Rehearso/Cell/dataDummy.swift
@@ -12,9 +12,9 @@ struct Dummy {
     var blnPresentasi : String
     var tglBuatCueCard : String
     var wktBuatCueCard : String
-    
 }
 
 var dataDummyRecent : [Dummy] = [
     Dummy(judul: "Presentasi Matematika", tglPresentasi: "29", blnPresentasi: "APR", tglBuatCueCard: "24/04/21", wktBuatCueCard: "09:31:21"),
-    Dummy(judul: "Acara Tunangan", tglPresentasi: "25", blnPresentasi: "MAR", tglBuatCueCard: "04/04/21", wktBuatCueCard: "12:31:21")]
+    Dummy(judul: "Acara Tunangan", tglPresentasi: "25", blnPresentasi: "MAR", tglBuatCueCard: "04/04/21", wktBuatCueCard: "12:31:21")
+]
