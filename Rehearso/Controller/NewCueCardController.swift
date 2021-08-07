bdd043db5e2aaf0fae5f0d2fc1c52d40e8114e92
Mohammad Sulthan <37440405+mohsulthana@users.noreply.github.com>
2021-08-07 11:41:16 +0700

    finish create presentation until sync to calendar

--- Rehearso/Controller/NewCueCardController.swift
+++ Rehearso/Controller/NewCueCardController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreData
+import EventKit
 
 class NewCueCardController: UIViewController {
 
@@ -14,13 +16,152 @@ class NewCueCardController: UIViewController {
     @IBOutlet weak var tfDuration: UITextField!
     @IBOutlet weak var tfDateOfPresentation: UITextField!
     @IBOutlet weak var syncToCalender: UISwitch!
+    @IBOutlet weak var createBtn: UIButton!
+    
+    private var editCue: Bool = false
+    private var cueCardUpdate: CueCard?
+    
+    let datePresentationPicker = UIDatePicker()
+    let eventStore = EKEventStore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        showDatePicker()
+        showDurationPicker()
         viewPresentationData.dropShadow()
+        if syncToCalender.isOn {
+            syncToCalender.setOn(true, animated: true)
+        }
+        syncToCalender.addTarget(self, action: #selector(askForCalendarPermission), for: .valueChanged)
     }
+    
+    func showDurationPicker() {
+        datePresentationPicker.datePickerMode = .countDownTimer
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.plain, target: self, action: #selector(self.doneDatePicker))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItem.Style.plain, target: self, action: #selector(self.cancelDatePicker))
+        
+        toolbar.setItems([doneButton, spaceButton, cancelButton], animated: true)
+        
+        tfDuration.inputAccessoryView = toolbar
+        tfDuration.inputView = datePresentationPicker
+    }
+    
     @IBAction func btnCreatePresentation(_ sender: Any) {
+        guard let cueName = tfPresentationName.text else {
+            print("Error presentation name")
+            return
+        }
+        
+        guard let date = tfDateOfPresentation.text else {
+            print("Error presentation date")
+            return
+        }
+        
+        guard let length = tfDuration.text else {
+            print("Error length")
+            return
+        }
+        
+        var calendarSynced = false
+        
+        // jika mode edit
+        if editCue {
+            cueCardUpdate?.name = cueName
+            cueCardUpdate?.date = date
+            cueCardUpdate?.length = length
+            CoreDataHelper.shared.save()
+            editCue = false
+            self.createBtn.setTitle("Save", for: .normal)
+        } else {
+            
+            // jika ingin sikronisasi dengan calendar
+            if syncToCalender.isOn {
+                syncWithCalendarAction()
+                calendarSynced = true
+            }
+            CoreDataHelper.shared.setCueCard(name: cueName, date: date, length: length, synced: calendarSynced)
+        }
+        
+        tfPresentationName.text = ""
+        tfDateOfPresentation.text = ""
+        tfDuration.text = ""
+    }
+    
+    func syncWithCalendarAction() -> Bool {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MMMM-yyyy"
+        
+        let event:EKEvent = EKEvent(eventStore: eventStore)
+        let startDate = dateFormatter.date(from: tfDateOfPresentation.text!)
+        let endDate = startDate!.addingTimeInterval(2 * 60 * 60)
+        
+        event.title = tfPresentationName?.text
+        event.startDate = startDate
+        event.endDate = endDate
+        event.notes = "Informative Presentation"
+        event.calendar = eventStore.defaultCalendarForNewEvents
+        
+        do {
+            try eventStore.save(event, span: .thisEvent)
+            print("Berhasil disinmapn")
+            return true
+        } catch let error as NSError {
+            print("Failed to save event with error: \(error)")
+            return false
+        }
+    }
+    
+    @objc func askForCalendarPermission() {
+        switch EKEventStore.authorizationStatus(for: .event) {
+        case .authorized:
+            print("Permission authorized")
+        case .notDetermined:
+            eventStore.requestAccess(to: .event, completion: { (granted: Bool, error: Error?) -> Void in
+                if granted {
+                    print("Granted")
+                } else {
+                    let alert = UIAlertController(title: "Permission denied", message: "Your schedule will never be syncrhonized with Calendar.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default))
+                }
+            })
+        case .denied:
+            let alert = UIAlertController(title: "Permission denied", message: "Your schedule will never be syncrhonized with Calendar.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Permission denied", style: .default))
+        case .restricted:
+            print("Event restricted")
+        @unknown default:
+            print("Case default")
+        }
+    }
+    
+    func showDatePicker() {
+        datePresentationPicker.datePickerMode = .dateAndTime
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.plain, target: self, action: #selector(self.doneDatePicker))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItem.Style.plain, target: self, action: #selector(self.cancelDatePicker))
+        
+        toolbar.setItems([doneButton, spaceButton, cancelButton], animated: true)
+        tfDateOfPresentation.inputAccessoryView = toolbar
+        tfDateOfPresentation.inputView = datePresentationPicker
+    }
+    
+    @objc func cancelDatePicker() {
+        self.view.endEditing(true)
+    }
+    
+    @objc func  doneDatePicker() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        tfDateOfPresentation.text = formatter.string(from: datePresentationPicker.date)
+        self.view.endEditing(true)
     }
 }
---
+++ Rehearso/Helper.swift
@@ -0,0 +1,127 @@
+//
+//  Helper.swift
+//  CoreDataRelation
+//
+//  Created by Martinus Galih Widananto on 02/08/21.
+//
+
+import Foundation
+import CoreData
+
+
+class CoreDataHelper {
+    static let shared = CoreDataHelper()
+    
+    lazy var coreDataHelper: NSPersistentContainer = {
+
+        let container = NSPersistentContainer(name: "CoreDataRelation")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    func setCueCard(name: String, date: String, length: String, synced: Bool) {
+        let cueCard = CueCard(context: coreDataHelper.viewContext)
+        cueCard.id = UUID()
+        cueCard.name = name
+        cueCard.date = date
+        cueCard.length = length
+        cueCard.syncToCalendar = synced
+        save()
+        print("\(cueCard)")
+    }
+    
+    func setSection(part: String, cueCard: CueCard) {
+        let section = Section(context: coreDataHelper.viewContext)
+        section.id = UUID()
+        section.part = part
+        section.cueCard = cueCard
+        save()
+    }
+    
+    func setIsi(part: String, isi: String, section: Section) {
+        let isii = Isi(context: coreDataHelper.viewContext)
+        isii.id = UUID()
+        isii.isi = isi
+        isii.part = part
+        isii.section = section
+        save()
+    }
+    
+    func fetchCueCard() -> [CueCard] {
+        let request: NSFetchRequest<CueCard> = CueCard.fetchRequest()
+        
+        var cueCard: [CueCard] = []
+        
+        do {
+            cueCard = try coreDataHelper.viewContext.fetch(request)
+        } catch {
+            print("Error fetching cuecard data")
+        }
+        
+        return cueCard
+    }
+    
+    func deleteCueCard(cueCard : CueCard) {
+        coreDataHelper.viewContext.delete(cueCard)
+        save()
+    }
+    
+    func deleteIsi(isi: Isi) {
+        coreDataHelper.viewContext.delete(isi)
+        save()
+    }
+    
+    func fetchSection(cueCard: CueCard) -> [Section] {
+        let request: NSFetchRequest<Section> = Section.fetchRequest()
+        
+        request.fetchOffset = 0
+        request.fetchLimit = 3
+        
+        request.predicate = NSPredicate(format: "(cueCard = %@)", cueCard)
+        request.sortDescriptors = [NSSortDescriptor(key: "part", ascending: false)]
+        var section: [Section] = []
+        
+        do{
+            section = try coreDataHelper.viewContext.fetch(request)
+        }catch {
+            print("Error fetching section data")
+        }
+        return section
+    }
+    
+    func fetchIsi(section: Section) -> [Isi] {
+        let request: NSFetchRequest<Isi> = Isi.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "(section = %@)", section)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "part", ascending: true)]
+        
+        var isi: [Isi] = []
+        
+        do{
+            isi = try coreDataHelper.viewContext.fetch(request)
+        }catch {
+            print("Error fetching isi data")
+        }
+        return isi
+    }
+    
+    func save () {
+        let context = coreDataHelper.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+}
